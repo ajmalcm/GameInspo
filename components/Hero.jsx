df0ce99b33e6cd092367cb05613eb19f8c1ff6df
ajmalcm <ajmalcm22@gmail.com>
2025-02-26 01:40:25 +0530

    done with tailwind config

---
+++ components/Hero.jsx
@@ -0,0 +1,36 @@
+
+'use client'
+import React, { useRef, useState } from "react";
+
+const Hero = () => {
+
+    const [currentindex,setCurrentIndex]=useState(1);
+    const [hasClicked,setHasClicked]=useState(false);
+    const [isLoading,setIsLoading]=useState(true);
+    const [loadedVideos,setLoadedVideos]=useState(0);
+
+    const totalVideos=4;
+    const nextVideoRef=useRef();
+
+    const handleMiniVdClick=()=>{
+        setHasClicked(true);
+        setCurrentIndex((prevIndex)=>prevIndex+1)
+    }
+
+  return (
+    <div className="relative h-dvh w-screen overflow-x-hidden">
+      <div
+        id="video-frame"
+        className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
+      >
+        <div>
+          <div className="mask-clip-path absoulte-center absolute z-50 size-64 cursor-pointer rounded-lg overflow-hidden">
+            <div>minivideoPlayer</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
